e827d5f6875a849291a8242462562f47e2bf61e3
Volaxar <volaxar@gmail.com>
2018-02-02 14:56:10 +0500

    Рефакторинг, изменение логики фильтрации.

--- shoplist/static/shoplist/js/script.js
+++ shoplist/static/shoplist/js/script.js
@@ -1,29 +1,32 @@
 $(function () {
 
     var $purchaseForm = $('#purchase-form');
+    var $purchaseFormContent = $('#purchase-form-content');
+    var $filterTimer;
+    var $isEditMode = false;
 
-    var disableOtherButtons = function () {
-        $('.edit-purchase').prop('disabled', true);
-        $('.del-purchase').prop('disabled', true);
+    // Переключение режимов: редактирование/просмотр
+    var switchEditMode = function (mode) {
+        $('.edit-purchase').prop('disabled', mode);
+        $('.del-purchase').prop('disabled', mode);
+        $('#purchase-filter').prop('disabled', mode);
+        $('#add-purchase').prop('disabled', mode);
 
-        $('#purchase-filter').prop('disabled', true);
-        $('#purchase-filter-button').prop('disabled', true);
+        $isEditMode = mode;
     };
 
     // Добавить покупку
-    $('#add-purchase').click(function (e) {
-        disableOtherButtons();
+    $('#add-purchase').click(function () {
+        switchEditMode(true);
 
-        if ($('#purchase-new-line').length === 0) {
-            $.get('/shoplist/create/', function (data) {
-                $('#purchase-list').append(data);
-            })
-        }
+        $.get('/shoplist/create/', function (data) {
+            $('#purchase-list').append(data);
+        })
     });
 
     // Редактировать покупку
-    $purchaseForm.on('click', '.edit-purchase', function (e) {
-        disableOtherButtons();
+    $purchaseForm.on('click', '.edit-purchase', function () {
+        switchEditMode(true);
 
         var purchaseLine = $(this).parent().parent();
         var pId = purchaseLine.data('pid');
@@ -34,13 +37,13 @@ $(function () {
     });
 
     //Удалить покупку
-    $purchaseForm.on('click', '.del-purchase', function (e) {
+    $purchaseForm.on('click', '.del-purchase', function () {
         var purchaseLine = $(this).parent().parent();
         var pId = purchaseLine.data('pid');
         var token = $purchaseForm.find('[name="csrfmiddlewaretoken"]').serialize();
 
         $.post('/shoplist/' + pId + '/delete/', token, function (data) {
-            $purchaseForm.html(data);
+            $purchaseFormContent.html(data);
         });
     });
 
@@ -50,48 +53,57 @@ $(function () {
 
         var actionUrl = '/shoplist/create/';
 
-        var $primaryKeyField = $('#id_id');
-        if ($primaryKeyField.attr('value')) {
-            actionUrl = '/shoplist/' + $primaryKeyField.attr('value') + '/';
+        var primaryKeyField = $('#id_id');
+        if (primaryKeyField.attr('value')) {
+            actionUrl = '/shoplist/' + primaryKeyField.attr('value') + '/';
         }
 
         var data = $purchaseForm.serialize();
 
         $.post(actionUrl, data, function (data) {
-            $purchaseForm.html(data)
+            $purchaseFormContent.html(data);
+            switchEditMode(false);
         });
     });
 
     // Отменить добавление покупки
-    $purchaseForm.on('click', '#cancel-purchase', function (e) {
+    $purchaseForm.on('click', '#cancel-purchase', function () {
         $.get('/shoplist/', function (data) {
-            $purchaseForm.html(data);
+            $purchaseFormContent.html(data);
+            switchEditMode(false);
         });
     });
 
     // Отфильтровать список
     var filterList = function () {
+        clearTimeout($filterTimer);
+
         var filter = $('#purchase-filter').serialize();
 
         $.get('/shoplist/', filter, function (data) {
-            $purchaseForm.html(data);
+            $purchaseFormContent.html(data);
         });
     };
 
-    $purchaseForm.on('click', '#purchase-filter-button', filterList);
     $purchaseForm.on('keypress', '#purchase-filter', function (e) {
         if (e.which === 13) {
+            e.preventDefault();
             filterList();
         }
+    });
+    $purchaseForm.on('input', '#purchase-filter', function () {
+        clearTimeout($filterTimer);
+        $filterTimer = setTimeout(filterList, 500);
     });
 
     // Сортировать список
     $purchaseForm.on('click', 'th[data-th-name]', function () {
+        if ($isEditMode) return;
+
         var thName = $(this).data('th-name');
 
         $.get('/shoplist/', 'order_by=' + thName, function (data) {
-            $purchaseForm.html(data);
+            $purchaseFormContent.html(data);
         });
     });
-
 });
