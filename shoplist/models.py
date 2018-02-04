600b2cfcfb1c1330f5a00a96f2fb196b50ccc4df
Volaxar <volaxar@gmail.com>
2018-02-04 08:32:07 +0500

    Рефакторинг моделей

--- shoplist/models.py
+++ shoplist/models.py
@@ -3,35 +3,35 @@ from django.db import models
 
 
 class Unit(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField('Название', max_length=15)
 
     def __str__(self):
         return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    color = models.PositiveIntegerField()
+    name = models.CharField('Название', max_length=50)
+    color = models.PositiveIntegerField('Цвет')
 
     def __str__(self):
         return self.name
 
 
 class Priority(models.Model):
-    name = models.CharField(max_length=50)
-    value = models.SmallIntegerField()
+    name = models.CharField('Название', max_length=50)
+    value = models.SmallIntegerField('Приоритет')
 
     def __str__(self):
         return self.name
 
 
 class Purchase(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Название', help_text='Название товара')
-    amount = models.IntegerField(verbose_name='Количество', help_text='Количество товара', default=1)
+    name = models.CharField('Название', max_length=250, help_text='Название товара')
+    amount = models.IntegerField('Количество', help_text='Количество товара', default=1)
     unit = models.ForeignKey(Unit, verbose_name='Ед. изм.', help_text='Единица измерения')
     category = models.ForeignKey(Category, verbose_name='Категория', help_text='Категория товара')
     priority = models.ForeignKey(Priority, verbose_name='Приоритет', help_text='Приориет покупки товара')
-    status = models.BooleanField(verbose_name='Статус', help_text='Товар куплен?')
+    status = models.BooleanField('Статус', help_text='Товар куплен?')
 
     def get_absolute_url(self):
         return reverse('purchase-list')
